5e416280d7416dc83d1b292ba769a8167c05fca6
TechDev-CEO <shoriful521399@gmail.com>
2022-03-16 16:34:44 +0600

    Firebase Complete

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBztvAWQ5u6nNmU7nf9Jby0ZaopEneqkH8",
+    authDomain: "burj-al-arab-firebase-425b1.firebaseapp.com",
+    projectId: "burj-al-arab-firebase-425b1",
+    storageBucket: "burj-al-arab-firebase-425b1.appspot.com",
+    messagingSenderId: "777939761539",
+    appId: "1:777939761539:web:3b8e21766f0db3ae755eb6"
+  };
+
+  export default firebaseConfig;
